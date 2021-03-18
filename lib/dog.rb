818946e3c521c929ab7dd07c70f86087df25bbd0
Lrd134 <larryc3200@gmail.com>
2021-03-18 14:12:36 -0400

    commit tree 4b05ee1d8d874b951d24d1613404b786dad8dec0

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
 # Add your code here
+class Dog
+    attr_accessor :name
+    @@all = []
+    def initialize(dog_name)
+        self.name = dog_name
+        save
+    end
+    def self.all
+        @@all
+    end
+    def self.clear_all
+        @@all = []
+    end
+    def self.print_all
+        @@all.each do | index |
+            puts index.name
+        end
+    end
+    def save
+        @@all << self
+    end
+end
